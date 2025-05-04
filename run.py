a9bd84e6dac630f3be6556fd2d6d33ed493354bc
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2025-05-04 12:08:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -92,8 +92,9 @@ def train_epoch(model, training_data, graph, hypergraph_list, loss_func, kt_loss
         loss, n_correct = get_performance(loss_func, pred, gold)
         loss_kt, auc, acc = kt_loss(pred_res, ans,
                                     kt_mask)  # ============================================================================
+        print("loss:", loss)
+        print("loss_kt:", loss_kt)
         loss = loss + loss_kt*3
-        # print("loss_kt:", loss_kt)
 
         loss.backward()
 
@@ -175,7 +176,7 @@ def train_model(MSHGAT, data_path):
                 print(metric + ' ' + str(scores[metric]))
             print('auc_test: {:.10f}'.format(np.mean(auc_test)),
                   'acc_test: {:.10f}'.format(np.mean(acc_test)))
-            if (validation_history <= sum(scores.values())) and (his_auc<=(np.mean(auc_test))) and (his_acc <=(np.mean(acc_test))):
+            if (validation_history <= sum(scores.values())) and ((his_auc<=(np.mean(auc_test))) or (his_acc <=(np.mean(acc_test)))):
                 print("Best Validation hit@100:{} at Epoch:{}".format(scores["hits@20"], epoch_i))
                 validation_history = sum(scores.values())
                 his_auc = np.mean(auc_test)
